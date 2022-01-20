7e1ccd2bbcf93f38122e8c960410b667384906eb
Nived P A <62166124+nivedwho@users.noreply.github.com>
2022-01-20 15:24:41 +0530

    Update losses.py

--- modules/losses.py
+++ modules/losses.py
@@ -13,24 +13,24 @@ def MultiBoxLoss(num_class=2, neg_pos_ratio=3):
         num_prior = tf.shape(y_true)[1]
 
         loc_pred = tf.reshape(y_pred[0], [num_batch * num_prior, 4])
-        landm_pred = tf.reshape(y_pred[1], [num_batch * num_prior, 10])
-        class_pred = tf.reshape(y_pred[2], [num_batch * num_prior, num_class])
+        #landm_pred = tf.reshape(y_pred[1], [num_batch * num_prior, 10])
+        class_pred = tf.reshape(y_pred[1], [num_batch * num_prior, num_class])
         loc_true = tf.reshape(y_true[..., :4], [num_batch * num_prior, 4])
-        landm_true = tf.reshape(y_true[..., 4:14], [num_batch * num_prior, 10])
-        landm_valid = tf.reshape(y_true[..., 14], [num_batch * num_prior, 1])
+        #landm_true = tf.reshape(y_true[..., 4:14], [num_batch * num_prior, 10])
+        #landm_valid = tf.reshape(y_true[..., 14], [num_batch * num_prior, 1])
         class_true = tf.reshape(y_true[..., 15], [num_batch * num_prior, 1])
 
         # define filter mask: class_true = 1 (pos), 0 (neg), -1 (ignore)
         #                     landm_valid = 1 (w landm), 0 (w/o landm)
         mask_pos = tf.equal(class_true, 1)
         mask_neg = tf.equal(class_true, 0)
-        mask_landm = tf.logical_and(tf.equal(landm_valid, 1), mask_pos)
+        #mask_landm = tf.logical_and(tf.equal(landm_valid, 1), mask_pos)
 
         # landm loss (smooth L1)
-        mask_landm_b = tf.broadcast_to(mask_landm, tf.shape(landm_true))
-        loss_landm = _smooth_l1_loss(tf.boolean_mask(landm_true, mask_landm_b),
-                                     tf.boolean_mask(landm_pred, mask_landm_b))
-        loss_landm = tf.reduce_mean(loss_landm)
+        #mask_landm_b = tf.broadcast_to(mask_landm, tf.shape(landm_true))
+        #loss_landm = _smooth_l1_loss(tf.boolean_mask(landm_true, mask_landm_b),
+        #                             tf.boolean_mask(landm_pred, mask_landm_b))
+        #loss_landm = tf.reduce_mean(loss_landm)
 
         # localization loss (smooth L1)
         mask_pos_b = tf.broadcast_to(mask_pos, tf.shape(loc_true))
@@ -69,6 +69,6 @@ def MultiBoxLoss(num_class=2, neg_pos_ratio=3):
             y_true=filter_class_true, y_pred=filter_class_pred)
         loss_class = tf.reduce_mean(loss_class)
 
-        return loss_loc, loss_landm, loss_class
+        return loss_loc, loss_class
 
     return multi_box_loss
